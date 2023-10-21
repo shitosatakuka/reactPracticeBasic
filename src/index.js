6b2f4e3a13cd7a00bdc0a598e717f22838b45bd9
kt3t4 <satoshi.kaku@gmail.com>
2023-10-21 00:09:36 +0000

    Update index.jsJSXは必ず一つのタグで囲わないといけないので、タグで囲ってあげる。ただし、不要なタグも生成されてしまうので、<React.Fragment></React.Fragment>で囲う     // 空のタグで囲うことでも問題なく動作する<></>

--- src/index.js
+++ src/index.js
@@ -2,7 +2,14 @@ import React from "react";
 import ReactDom from "react-dom";
 
 const App = () => {
-  return null;
+  return (
+    // JSXは必ず一つのタグで囲わないといけないので、タグで囲ってあげる。ただし、不要なタグも生成されてしまうので、<React.Fragment></React.Fragment>で囲う
+    // 空のタグで囲うことでも問題なく動作する<></>
+    <React.Fragment>
+      <h1>Hello World!</h1>
+      <p>おげんきですか？</p>
+    </React.Fragment>
+  );
 };
 
 ReactDom.render(<App />, document.getElementById("root"));
