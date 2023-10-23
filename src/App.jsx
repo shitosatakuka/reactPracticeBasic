b9f1f0e27fab9719a054e418a1a58ca4d47a548e
kt3t4 <satoshi.kaku@gmail.com>
2023-10-23 23:27:35 +0000

    Update App.jsx ボタンに関数を入れて、反応させる

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 
 const App = () => {
+  const onClickButton = () => alert();
   return (
     // JSXは必ず一つのタグで囲わないといけないので、タグで囲ってあげる。ただし、不要なタグも生成されてしまうので、<React.Fragment></React.Fragment>で囲う
     // 空のタグで囲うことでも問題なく動作する<></>
     <>
       <h1>Hello World!</h1>
       <p>おげんきですか？</p>
+      {/* ボタンをつける。動作を指定するときに関数書き込む */}
+      <button onClick={onClickButton}>ボタン</button>
     </>
   );
 };
